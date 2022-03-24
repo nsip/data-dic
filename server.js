6a4ff4f2fcc4bef13f732c9441f7755ddc034a7c
Qing Miao <digisanQM@outlook.com>
2022-03-24 16:38:58 +1100

    * add swagger

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ import fastifyFac from 'fastify'
 import multipart from 'fastify-multipart'
 import formbody from 'fastify-formbody'
 import stat from 'fastify-static'
+import fastifyswagger from 'fastify-swagger'
 
 import { config } from './config.js'
 import { esa_dic } from './api/page-render.js'
+import { swagger_obj } from './swagger.js';
+import { dic_api } from './api/api.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,6 +26,14 @@ fastify.register(esa_dic)
 fastify.register(stat, {
     root: path.join(__dirname, 'www'),
     prefix: '/', // optional: default '/'
+})
+
+// --- swagger --- //
+fastify.register(fastifyswagger, swagger_obj)
+fastify.register(dic_api)
+fastify.ready(err => {
+    if (err) throw err
+    fastify.swagger()
 })
 
 // --- run the server --- //
